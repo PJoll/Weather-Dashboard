8467ab7ead3064e3ed73806250b9a35553fea774
Peter Jolley <peterjolley456@gmail.com>
2022-04-14 19:27:37 +0930

    Changes made to js

--- Assets/display-search.js
+++ Assets/display-search.js
@@ -1,5 +1,6 @@
-
-
+var resultTextE1 = $("#result-text");
+var cityContainerE1 = $("#city-container");
+var searchFormE1 = $("#search-form")
 
 
 
@@ -8,4 +9,40 @@ function getParams() {
     var query = searchParamsArr[0].split("=").pop()
     
     searchApi(query);
+}
+
+function printResults(resultObj) {
+    console.log(resultObj);
+
+var resultsCard = document.createElement("div")
+resultsCard.classList.add("card", "bg-light", "text-dark");
+ 
+var resultsBody = document.createElement("div");
+resultsCard.classList.add("card-body");
+resultsCard.append(resultsBody);
+ 
+
+// add bold title
+var titleE1 = document.createElement('h3');
+titleE1.textContent = resultObj.titel;
+
+var bodyContentE1 = document.createElement('p');
+bodyContentE1.textContent = resultObj.bodyContent;
+resultsBody.append(titleE1, bodyContentE1);
+cityContainerE1.append(resultsCard);
+}
+
+const makeRequest = async () => {
+    const response =   await
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
+}
+makeRequest()
+
+
+
+
+
+
+
+
 }
