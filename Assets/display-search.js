dc616ea57d4a3f08360faf32ce7c8b79ffc9ad4c
Peter Jolley <peterjolley456@gmail.com>
2022-04-15 17:01:18 +0930

    attempted the js logic running the fetch with the api

--- Assets/display-search.js
+++ Assets/display-search.js
@@ -1,48 +1,48 @@
-var resultTextE1 = $("#result-text");
-var cityContainerE1 = $("#city-container");
-var searchFormE1 = $("#search-form")
+// var resultTextE1 = $("#result-text");
+// var cityContainerE1 = $("#city-container");
+// var searchFormE1 = $("#search-form")
 
 
 
-function getParams() {
-    var searchParamsArr = document.location.search.split("&");
-    var query = searchParamsArr[0].split("=").pop()
+// function getParams() {
+//     var searchParamsArr = document.location.search.split("&");
+//     var query = searchParamsArr[0].split("=").pop()
     
-    searchApi(query);
-}
+//     searchApi(query);
+// }
 
-function printResults(resultObj) {
-    console.log(resultObj);
+// function printResults(resultObj) {
+//     console.log(resultObj);
 
-var resultsCard = document.createElement("div")
-resultsCard.classList.add("card", "bg-light", "text-dark");
+// var resultsCard = document.createElement("div")
+// resultsCard.classList.add("card", "bg-light", "text-dark");
  
-var resultsBody = document.createElement("div");
-resultsCard.classList.add("card-body");
-resultsCard.append(resultsBody);
+// var resultsBody = document.createElement("div");
+// resultsCard.classList.add("card-body");
+// resultsCard.append(resultsBody);
  
 
-// add bold title
-var titleE1 = document.createElement('h3');
-titleE1.textContent = resultObj.titel;
+// // add bold title
+// var titleE1 = document.createElement('h3');
+// titleE1.textContent = resultObj.titel;
 
-var bodyContentE1 = document.createElement('p');
-bodyContentE1.textContent = resultObj.bodyContent;
-resultsBody.append(titleE1, bodyContentE1);
-cityContainerE1.append(resultsCard);
-}
+// var bodyContentE1 = document.createElement('p');
+// bodyContentE1.textContent = resultObj.bodyContent;
+// resultsBody.append(titleE1, bodyContentE1);
+// cityContainerE1.append(resultsCard);
+// }
 
-const makeRequest = async () => {
-    const response =   await
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
-}
-makeRequest()
-
-
+// const makeRequest = async () => {
+//     const response =   await
+//     fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
+// }
+// makeRequest()
 
 
 
 
 
 
-}
+
+
+// }
--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,72 @@
 const Apikey = "";
 
-const textInput =$("#input-city")
-function handlerSearchFormSubmit(event) {
-    event.preventDefault();
+const textInput = document.querySelector("#input-city");
+const button = document.querySelector("#input-search");
+const inputWrap = document.querySelector(".inputs");
+const loading = document.querySelector("#loading");
+const error =  document.querySelector("#error");
+const result = document.querySelector("#result")
 
-
-var searchInputVal = $("#City").value;
-
-if (!searchInputVal) {
-   alert("Please type a City");
+const getLatLng = async ( city ) => {
+    const result = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${ city }&limit=1&appid=${ ApiKey }` )
+        if (result.lenght) {
+            return{
+            success: true,
+            lat: result [ 0 ]?.lat,
+            lng: result [ 0 ]?.lon, 
+            }
+        } else {
+            return{
+                success: false,
+            }
+        }
+}
+const getWeather = async ( lat, lng ) => {
+    const result = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&appid=${ ApiKey }` )
+return result?.weather?.main || "Not Found"     
 }
 
-var queryString = "./search-results.html?q=" + searchInputVal;
+button.addEventListener( "click", async (e) => {
+    e.preventDefault()
+    inputWrap.style.display = "none"
+    error.style.display = "none"
+    loading.style.diplay = "block"
 
-location.assign(queryString);
-}
+    const city = textInput.value
 
-searchFormE1.addEventListener("submit", handlerSearchFormSubmit)
+    const coords = await getLatLng(city)
+    if (!coords.success ||
+        coords.lat === false ||
+        !coords.lng === false
+        ){
+            error.style.diplay = "block"
+            loading.style.diplay = "none"
+            return
+        }
+} )
+
+
+
+// 
+
+
+
+
+// function handlerSearchFormSubmit(event) {
+//     event.preventDefault();
+
+
+// var searchInputVal = $("#City").value;
+
+// if (!searchInputVal) {
+//    alert("Please type a City");
+// }
+
+// var queryString = "./search-results.html?q=" + searchInputVal;
+
+// location.assign(queryString);
+// }
+
+// searchFormE1.addEventListener("submit", handlerSearchFormSubmit)
