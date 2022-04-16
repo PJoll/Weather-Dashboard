0971f38cca69dc82bb5ff9641dc31a6aac9306be
Peter Jolley <peterjolley456@gmail.com>
2022-04-16 13:48:48 +0930

    fixed api key and link ready for results

--- Assets/script.js
+++ Assets/script.js
@@ -1,34 +1,37 @@
-const Apikey = "";
+var ApiKey = "5552ed791afd8968c2385d0dab7c508d";
 
 const textInput = document.querySelector("#input-city");
 const button = document.querySelector("#input-search");
 const inputWrap = document.querySelector(".inputs");
 const loading = document.querySelector("#loading");
-const error =  document.querySelector("#error");
+const error = document.querySelector("#error");
 const result = document.querySelector("#result")
 
-const getLatLng = async ( city ) => {
+const getLatLng = async (city) => {
     const result = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${ city }&limit=1&appid=${ApiKey}` )
-        if (result.lenght) {
-            return{
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${ApiKey}`)
+
+    const jSon = await result.json()
+    if (jSon.length) {
+        return {
             success: true,
-            lat: result [ 0 ]?.lat,
-            lng: result [ 0 ]?.lon, 
-            }
-        } else {
-            return{
-                success: false,
-            }
+            lat: jSon[0]?.lat,
+            lng: jSon[0]?.lon,
         }
+    } else {
+        return {
+            success: false,
+        }
+    }
 }
-const getWeather = async ( lat, lng ) => {
+const getWeather = async (lat, lng) => {
     const result = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&appid=${ ApiKey }` )
-return result?.weather?.main || "Not Found"     
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${ApiKey}`)
+    const jSon = await result.json()
+    return jSon || "Not Found"
 }
 
-button.addEventListener( "click", async (e) => {
+button.addEventListener("click", async (e) => {
     e.preventDefault()
     inputWrap.style.display = "none"
     error.style.display = "none"
@@ -37,19 +40,22 @@ button.addEventListener( "click", async (e) => {
     const city = textInput.value
 
     const coords = await getLatLng(city)
+
     if (!coords.success ||
         coords.lat === false ||
-        !coords.lng === false
-        ){
-            error.style.diplay = "block"
-            loading.style.diplay = "none"
-            return
-        }
-} )
+        coords.lng === false
+    ) {
+        error.style.diplay = "block"
+        loading.style.diplay = "none"
+        return
+    }
+    const weather = await getWeather(coords.lat, coords.lng)
+
+})
 
 
 
-// 
+//
 
 
 
